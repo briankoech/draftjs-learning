906df4c8bf3d4e7285af60f671f42497ee55fee3
Brian Koech <brnkirwa@gmail.com>
2016-03-21 20:20:43 +0300

    Add styling to controls

--- app/components/Home.js
+++ app/components/Home.js
@@ -33,12 +33,25 @@ const BLOCK_TYPES = [
   {label: 'OL', style: 'ordered-list-item'},
   {label: 'Code Block', style: 'code-block'},
 ];
-const Controls = (props) => {
+const BlockStyleControls = (props) => {
+  const { editorState } = props;
+  const selection = editorState.getSelection();
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey))
+    .getType();
+    
   return (
     <div className="RichEditor-controls">
       {
         BLOCK_TYPES.map((type) => {
-          <>
+          <StyleButton
+            key={type.label}
+            active={type.style}
+            label={type.label}
+            onToggle={props.onToggle}
+            style={type.style}
+          />
         })
       }
     </div>
